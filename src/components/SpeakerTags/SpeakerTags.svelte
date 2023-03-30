7847ef3871f850fcf4fcbfc679e041c322083ed5
Dmitry <gh1@ma23.ru>
2023-03-30 08:54:41 +0300

    SPEEC-1365: upd SpeakerTags

--- src/components/SpeakerTags/SpeakerTags.svelte
+++ src/components/SpeakerTags/SpeakerTags.svelte
@@ -13,8 +13,10 @@ $: tagsStr = speaker.meta_tags.join(', ')
 </script>
 
 <div class="speaker-tags">
-    <button class="speaker-tags__trigger" on:click={onClick}>?</button>
+    <button class="speaker-tags__trigger" on:click={onClick}>
+        <div>?</div>
+    </button>
     {#if showList}
-        <div class="speaker-tags__list" on:click={onClick}>{tagsStr}</div>
+        <div class="speaker-tags__list" title={tagsStr} on:click={onClick}>{tagsStr}</div>
     {/if}
 </div>
